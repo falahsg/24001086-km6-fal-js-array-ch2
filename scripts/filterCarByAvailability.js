f3bc48a100ce3264798a83c71f5c0edc7e6e2fa5
Falahsg <falahsg18@gmail.com>
2024-03-08 13:14:16 +0700

    first init

---
+++ scripts/filterCarByAvailability.js
@@ -0,0 +1,26 @@
+function filterCarByAvailability(cars) {
+  //error handler using try catch
+  try {
+    //declare result variable
+    const result = [];
+    //show unfiltered cars
+    console.info({
+      unfiltered_cars: cars,
+      unfiltered_cars_availabelity: cars.map((car) => car.available),
+    });
+    //loop each car then filter it by availability
+    cars.forEach((car) => car.available && result.push(car));
+    //show unfiltered cars
+    console.info({
+      filtered_cars: result,
+      filtered_cars_availabelity: result.map((car) => car.available),
+    });
+    //return the result
+    return result;
+
+    // return [...cars.filter(car => car.available)]
+  } catch (err) {
+    //show error message
+    console.error(err);
+  }
+}
---
+++ scripts/sortCarByYearAscendingly.js
@@ -0,0 +1,32 @@
+function sortCarByYearAscendingly(cars) {
+  //error handler using try catch
+  try {
+    //declare result with spread operator to avoid side effect
+    const result = [...cars];
+    //show unsorted cars
+    console.info({
+      unsorted_cars: cars,
+      unsorted_cars_years: cars.map((car) => car.year),
+    });
+    //sort result array by year ascendingly using bubble sort algorithm
+    for (let i = 0; i < result.length - 1; i++) {
+      for (let j = 0; j < result.length - 1; j++) {
+        if (result[j].year > result[j + 1].year) {
+          const temp = result[j];
+          result[j] = result[j + 1];
+          result[j + 1] = temp;
+        }
+      }
+    }
+    //show sorted cars
+    console.info({
+      sorted_cars: result,
+      sorted_cars_years: result.map((car) => car.year),
+    });
+    return result;
+
+    // return [...cars].sort((a,b) => a.year-b.year);
+  } catch (err) {
+    console.error(err);
+  }
+}
